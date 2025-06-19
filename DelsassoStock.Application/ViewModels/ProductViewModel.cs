66355a6096929ad9799a7ab09922d77dc1c4d113
GabriellDelsasso <gabrielldelsasso@hotmail.com>
2025-06-19 14:43:51 -0300

    Criando controller de produto e adicionando Endpoints

---
+++ DelsassoStock.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace DelsassoStock.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; } = String.Empty;
+        public Decimal Price { get; set; }
+    }
+
+    public class ProductViewModelValidator : AbstractValidator<ProductViewModel>
+    {
+        public ProductViewModelValidator() 
+        {
+            RuleFor(productViewModel => productViewModel.Name)
+                .NotEmpty()
+                .WithMessage("O Nome é obrigatório");
+
+            RuleFor(productViewModel => productViewModel.Price)
+                .NotEmpty()
+                .WithMessage("O Preço é obrigatório");
+        }
+    }
+}
---
+++ DelsassoStock/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using DelsassoStock.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelsassoStock.Controllers
+{
+    [Route("Product")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpPost("RegisterProduct")]
+        public async Task<ActionResult> RegisterProduct([FromBody] ProductViewModel productViewModel)
+        {
+
+        }
+
+        [HttpGet("GetAllProducts")]
+        public async Task<ActionResult> GetAllProducts()
+        {
+
+        }
+
+        [HttpPost("EditProduct")]
+        public async Task<ActionResult> EditProduct(Guid id, [FromBody] ProductViewModel productViewModel)
+        {
+
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public async Task<ActionResult> DeleteProduct(Guid id)
+        {
+
+        }
+    }
+}
